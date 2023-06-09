1da845c63d7a38ad4aaf1000c74e5893b7cbd3a3
Anurag1008 <julyanurag@gmail.com>
2023-06-09 12:24:48 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//jQuery $ function will only execute when document is ready.
+$(function(){
+    init();
+    console.log("Main Init Called");
+});     
+function init(){
+    console.log("init() called");
+}
