85732e8c9e3cbfd96d1c7ba4435635bf899037e0
Anurag1008 <julyanurag@gmail.com>
2023-06-15 14:47:42 +0530

    Generate a Poskey

--- js/board.js
+++ js/board.js
@@ -15,4 +15,28 @@ GameBoard.material = new Array(2); //white and black material of pieces.
 GameBoard.pceNum = new Array(13); //index by piece
 GameBoard.pList = new Array(14*10);
 GameBoard.posKey = 0;
+
+function GeneratePosKey(){
+    var sq = 0;
+    var finalKey = 0;
+    var piece = PIECES.EMPTY;
+
+    for(sq = 0; sq<BRD_SQ_NUM;++sq){
+        piece = GameBoard.pieces[sq];
+        if(piece != PIECES.EMPTY && piece != SQUARES.OFFBOARD){
+            finalKey ^= PieceKeys[(piece *120)+sq];
+        }
+    }
+    if(GameBoard.side == COLOURS.WHITE){
+        finalKey ^= SideKey;
+    }
+
+    if(GameBoard.enPas != SQUARES.NO_SQ){
+        finalKey ^= PieceKeys[GameBoard.enpas];
+    }
+
+    finalKey ^= CastleKeys][GameBoard.castlePerm];
+    
+    return finalKey;
+}
  
